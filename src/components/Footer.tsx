c5fbe301a985cc4277a688d8565465b85494aa4b
Henrique Fernandes <87838554+henriferi@users.noreply.github.com>
2025-07-11 11:35:43 -0300

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -70,7 +70,9 @@ const Footer: React.FC = () => {
           <p className="text-gray-400 flex items-center justify-center space-x-2">
             <span>© 2025 Aninha Festas</span>
             <Heart className="w-4 h-4 text-pink-500" />
-            <span>Criado e mantido por - <a href="https://hfernandes.dev.br">Fernandes Dev</a></span>
+            <span>Criado e mantido por - <a class="hover:underline decoration-wavy decoration-from-font decoration-gradient-to-r from-purple-500 via-purple-300 to-blue-500" target="blank" href="https://hfernandes.dev.br">Fernandes Dev
+              </a>
+            </span>
           </p>
         </div>
       </div>
